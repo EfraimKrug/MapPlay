74269a443b83649a18a84b4dc7f17b70d253ab7c
Efraim Krug <EfraimMKrug@GMail.com>
2016-07-28 13:20:28 -0400

    everything works here

--- php/getStuff.php
+++ php/getStuff.php
@@ -30,37 +30,42 @@ if(count($returnArray['DataSet']) > 0){
 	foreach($returnArray['DataSet'] as $idx => $row){
 		$collectionArray[$row['area_name']] =
 											array('center' =>
-														array('lat' => $row['lat'], 'lng' => $row['lng']),
-														'population' => $row['count']
+														array('lat' => (double)$row['lat'], 'lng' => (double)$row['lng']),
+														'population' => (int)$row['count']
 													);
 		}
 }
 
 #print_r($collectionArray);
 #echo json_encode($collectionArray);
+#exit(1);
 
-$arr = array(
-				'gardner' =>
-						array('center' =>
-								array('lat' => 42.338, 'lng' => -71.09),
-								'population' => 2
-						),
-				'parsons' =>
-						array('center' =>
-								array('lat' => 42.337, 'lng' => -71.115),
-								'population' => 5
-						),
-				'kenmore' =>
-						array('center' =>
-								array('lat' => 42.349, 'lng' => -71.096),
-								'population' => 3
-						),
-				'childrens' =>
-						array('center' =>
-								array('lat' => 42.337, 'lng' => -71.105),
-								'population' => 1
-						),
-			);
+// $arr = array(
+// 				'gardner' =>
+// 						array('center' =>
+// 								array('lat' => 42.338, 'lng' => -71.09),
+// 								'population' => 2
+// 						),
+// 				'parsons' =>
+// 						array('center' =>
+// 								array('lat' => 42.337, 'lng' => -71.115),
+// 								'population' => 5
+// 						),
+// 				'kenmore' =>
+// 						array('center' =>
+// 								array('lat' => 42.349, 'lng' => -71.096),
+// 								'population' => 3
+// 						),
+// 				'childrens' =>
+// 						array('center' =>
+// 								array('lat' => 42.337, 'lng' => -71.105),
+// 								'population' => 1
+// 						),
+// 			);
+
 #print_r($arr);
-echo json_encode($arr);
+echo json_encode($collectionArray);
+#echo "[" . json_encode($collectionArray) . "]";
+#echo "\n\n";
+#echo "[" . json_encode($arr) . "]";
 ?>
